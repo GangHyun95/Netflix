8776591e02fda2eea01bddd1eebc796328c6a4dc
hyun <hgh6128@gmail.com>
2023-03-28 00:29:03 +0900

    이미지 경로 설정 함수, Home 컴포넌트 작업 중

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,16 +1,64 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import styled from "styled-components";
 import { getMovies, IGetMoviesResult } from "../api";
+import { makeImagePath } from "../utils";
+
+const Wrapper = styled.div`
+  background: black;
+`;
+
+const Loader = styled.div`
+  height: 20vh;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Banner = styled.div<{ bgPhoto: string }>`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  padding: 60px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+    url(${(props) => props.bgPhoto});
+  background-size: cover;
+`;
+
+const Title = styled.h2`
+  font-size: 68px;
+  margin-bottom: 20px;
+`;
+
+const Overview = styled.p`
+  width: 50%;
+  font-size: 36px;
+`;
 
 const Home = () => {
-  const {
-    error,
-    isLoading,
-    data: movies,
-  } = useQuery<IGetMoviesResult>(["movies", "nowPlaying"], getMovies);
-  console.log(movies);
+  const { error, isLoading, data } = useQuery<IGetMoviesResult>(
+    ["movies", "nowPlaying"],
+    getMovies
+  );
+  console.log(data);
 
-  return <div style={{ height: "200vh" }}>Home</div>;
+  return (
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+        </>
+      )}
+    </Wrapper>
+  );
 };
 
 export default Home;
